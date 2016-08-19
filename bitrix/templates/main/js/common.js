2a3ed088518a144b538ed4afa2512d5da0f70d38
sweelemk <sweelemk@gmail.com>
2016-08-19 23:41:32 +0300

    create footer and write media styles

--- bitrix/templates/main/js/common.js
+++ bitrix/templates/main/js/common.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
 
-	// go js
+	// catalog
+	function catalogToggle() {
+		var nav = $(".nav"),
+			trigger = nav.find(".nav-trigger"),
+			container = nav.find(".nav-drop");
+		
+		trigger.on("mouseenter", function () {
+			nav.addClass("open")
+			container.fadeIn(150)
+		});
+
+		nav.on("mouseleave", function(){
+			nav.removeClass("open");
+			container.fadeOut(0);
+		});
+	} catalogToggle();
+
+	// wrapper top
+	function wrapperTop(){
+		var header = $(".header").innerHeight(),
+			wrapper = $(".wrapper");
+
+		wrapper.css("padding-top", header);
+	};
+
+	$(window).on("load resize", function(){
+		wrapperTop();
+	});
 
 });
